69df1bacdf5bfb2872d071a2911a638a52b046db
Natalie Finnegan <nfinneg2@gmail.com>
2021-02-14 17:04:16 -0700

    initial work on generatePassword function

--- script.js
+++ script.js
@@ -4,74 +4,54 @@ var specialCharacters = ["!","/","$","%","&","(", ")", "*", "+", "-", ".", ":","
 var upperCaseChar = ['A','B', 'C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCaseChar = upperCaseChar.map(upperCaseChar => upperCaseChar.toLowerCase());
 var num = ['0','1','2','3','4','5','6','7','8','9'];
-var passwordLength;
+var passwordLength; //intialize as an empty global variable
 var minlength = 8;
 var maxlength = 128;
-var passOptions = []
+var passOptions = [] //empty array will be used for the generated password results
+var confUpperCase; // to store input from user upon selection
+var confLowerCase; // to store input from user upon selection
+var confSpecialChar; // to store input from user upon selection
+var confNum; // to store input from user upon selection
 
 // Write password to the #password input
 
-// function generatePassword (
-//   passwordLength,
-//   specialCharacters,
-//   upperCaseChar,
-//   lowerCaseChar,
-//   num) {
-//     return "TEST"
-//   };
-
-
-var password = generatePassword();
-
-let confUpperCase = true;
-let confSpecialChar = true;
-let userInput = passwordLength;
-
-function generatePassword(
-  userInput,
-  confSpecialChar,
-  confUpperCase) 
-  {
-  return "TEST"
-}
 
 var passwordText = document.querySelector("#password");
+var password = generatePassword();
+passwordText.value = password;
 
+function generatePassword() {
+  console.log(passwordLength);
+  if (confUpperCase === true && confSpecialChar === true) {
+  for (i=0; i < passwordLength; i++ ) {
+  let results = math.floor(math.random() * 50);
+  passOptions += results.concat(upperCaseChar, specialCharacters);
+}}
+return passOptions
+};
 
-  // passwordText.value = password;
-  
-
-
+generatePassword(passwordLength)
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function writePassword() {
-var passwordLength = window.prompt("Choose password length between 8 and 128 characters");
-if (passwordLength > minlength && passwordLength < maxlength) {
-  confirm("Would you like to include uppercase letters?");
+passwordLength = Number(prompt("Choose password length between 8 and 128 characters"));
+  if (passwordLength > minlength && passwordLength < maxlength) {
+ confUpperCase = confirm("Would you like to include uppercase letters?");
   // confirm("Would you like to include lowercasce letters?");
-  confirm("Would you like to include special characters?");
+ confSpecialChar = confirm("Would you like to include special characters?");
   // confirm("Would you like to include numbers?");
 }  
-else if (passwordLength < minlength || passwordLength > maxlength) {
+  else if (passwordLength < minlength || passwordLength > maxlength) {
     alert("Must meet length requirements!");
   }
-else if (passwordLength = "null" || passwordLength === (lowerCaseChar || upperCaseChar)) {
+  else if (passwordLength = "null" || passwordLength === (lowerCaseChar || upperCaseChar)) {
     alert("Please choose a number between 8-128");
   }
-else {
+  else {
    alert("Please start over")
 }
  
 };
 
-
-
-// writePassword();
-generatePassword ();
-
-// var confUpperCase = confirm("Would you like to include uppercase letters?");
-// var confLowerCase = confirm("Would you like to include lowercasce letters?");
-// var confSpecialChar = confirm("Would you like to include special characters?");
-// var confNum = confirm("Would you like to include numbers?");
-// var userInput = passwordLength
+//var userInput = passwordLength
